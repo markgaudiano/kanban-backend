c527c4f90b1e2bfff410ba86ffafd3e178a13684
Markoz <gaudianomark1@gmail.com>
2020-11-19 10:40:53 +0800

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc224152de8f085de6a75c1f95ac83d4'),
+    },
+  },
+});
